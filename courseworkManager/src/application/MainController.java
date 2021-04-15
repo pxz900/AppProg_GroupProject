13867cc29e45414667dd1566c94ba766a360163a
Zach True <78751985+pxz900@users.noreply.github.com>
2021-04-14 19:24:42 -0500

    Add files via upload

---
+++ courseworkManager/src/application/MainController.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainController {
+	
+	
+	@FXML
+	private AnchorPane mainPane;
+	
+	//handles button to take you to Add Scene
+	@FXML
+	public void handleAdd(ActionEvent event) throws IOException{
+		mainPane = FXMLLoader.load(getClass().getResource("Add.fxml"));
+		Scene scene = new Scene(mainPane);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+	
+	
+}
